fd6c8454d3dfb0cb25b69d4c7aafb4833f110ec9
Davide Caruso <davide.caruso@immedia.net>
2017-08-16 14:50:02 +0200

    Rename Hello component to Home

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'This is the homepage'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
